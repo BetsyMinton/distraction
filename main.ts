20f24c933682e87655c8848b3018390e4b14bb49
emm2305 <50798469+emm2305@users.noreply.github.com>
2020-03-09 18:35:04 -0500

    working on adding a level

--- main.ts
+++ main.ts
@@ -1,34 +1,165 @@
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    choice = 3
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    choice = 0
+    mySprite.setImage(arrows[choice])
     checkChoice()
 })
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    choice = 1
-    checkChoice()
-})
-function checkChoice () {
-    if (choice == arrowList.shift()) {
-        info.changeScoreBy(1)
-        if (arrowList.length == 0) {
-            info.stopCountdown()
-        }
+function goToLevel () {
+    if (whichLevel == 0) {
+        BaseLineLevel()
     } else {
-        game.over(false)
+        if (whichLevel == 1) {
+            MusicLevel()
+            Song()
+        } else {
+            if (whichLevel == 2) {
+            	
+            } else {
+                if (whichLevel == 3) {
+                	
+                }
+            }
+        }
     }
 }
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+function checkChoice () {
+    if (choice == arrowList.shift()) {
+        points += 1
+        console.log(arrowList)
+        if (arrowList.length == 0) {
+            Level_Over()
+        }
+    } else {
+        points += -1
+        console.log(arrowList)
+        if (arrowList.length == 0) {
+            Level_Over()
+        }
+    }
+}
+function Song () {
+    notesList = [262, 294, 330, 392, 440, 523, 220]
+    pause(500)
+    while (list.length != 0) {
+        music.playTone(notesList[Math.randomRange(0, 6)], music.beat(BeatFraction.Half))
+    }
+}
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    choice = 3
+    mySprite.setImage(arrows[choice])
+    checkChoice()
+})
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    choice = 1
+    mySprite.setImage(arrows[choice])
+    checkChoice()
+})
+function Level_Over () {
+    mySprite.setImage(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`)
+    Timer = (game.runtime() - Start_time) / 1000
+    list.push(points)
+    game.splash("" + Math.round(points / Timer * 100) + " " + "points")
+    whichLevel += 1
+    goToLevel()
+}
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
     choice = 2
+    mySprite.setImage(arrows[choice])
     checkChoice()
 })
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    choice = 0
-    checkChoice()
-})
-let choice = 0
+function MusicLevel () {
+    game.showLongText("Carefully watch the arrows,   remember the order in which they appear", DialogLayout.Center)
+    for (let index = 0; index < 7; index++) {
+        ChooseArrow = Math.randomRange(0, 3)
+        mySprite.setImage(arrows[ChooseArrow])
+        arrowList.push(ChooseArrow)
+        pause(1000)
+        mySprite.setImage(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`)
+        pause(100)
+    }
+    console.log(arrowList)
+    points = 0
+    game.showLongText("Use the keypad to recreate the pattern", DialogLayout.Center)
+    Start_time = game.runtime()
+}
+function BaseLineLevel () {
+    game.showLongText("Carefully watch the arrows,   remember the order in which they appear", DialogLayout.Center)
+    for (let index = 0; index < 7; index++) {
+        ChooseArrow = Math.randomRange(0, 3)
+        mySprite.setImage(arrows[ChooseArrow])
+        arrowList.push(ChooseArrow)
+        pause(1000)
+        mySprite.setImage(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`)
+        pause(100)
+    }
+    console.log(arrowList)
+    points = 0
+    game.showLongText("Use the keypad to recreate the pattern", DialogLayout.Center)
+    Start_time = game.runtime()
+}
 let ChooseArrow = 0
+let Start_time = 0
+let notesList: number[] = []
+let choice = 0
+let whichLevel = 0
+let Timer = 0
+let points = 0
 let arrowList: number[] = []
+let list: number[] = []
+let arrows: Image[] = []
+let mySprite: Sprite = null
 scene.setBackgroundColor(9)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -46,7 +177,7 @@ let mySprite = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
-let arrows = [img`
+arrows = [img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . f f f . . . . . . . 
@@ -115,31 +246,10 @@ let arrows = [img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `]
+list = []
 arrowList = []
-for (let index = 0; index < 5; index++) {
-    ChooseArrow = Math.randomRange(0, 3)
-    mySprite.setImage(arrows[ChooseArrow])
-    arrowList.push(ChooseArrow)
-    pause(1000)
-    mySprite.setImage(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`)
-    pause(100)
-}
-info.startCountdown(10)
-console.log(arrowList)
+let Score = 0
+points = 0
+Timer = 0
+whichLevel = 0
+goToLevel()
